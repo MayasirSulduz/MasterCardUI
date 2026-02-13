f3156cc5f51c9c1ccce60ce45cd0986048f78495
MayasirSulduz <shaiksulduz238@gmail.com>
2026-02-13 09:13:01 +0530

    Initial commit

---
+++ src/components/Atm.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function Atm() {
+ 
+    return (
+        // <div className="text-4xl text-blue-500">
+        <div className="min-h-screen flex items-center justify-center text-4xl text-blue-500">
+            ATM component
+        </div>
+
+    )
+}
+
+export default Atm;;
