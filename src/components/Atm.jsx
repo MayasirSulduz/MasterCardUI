ed60fc7a910a5c7f223d113ddb010cd685fa82b0
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2026-02-24 11:32:03 +0530

    Added middle and bottom row

--- src/components/Atm.jsx
+++ src/components/Atm.jsx
@@ -5,15 +5,32 @@ function Atm() {
     return (
         // <div className="text-4xl text-blue-500">
         // <div className="min-h-screen flex items-center justify-center text-4xl text-blue-500">
-        <div className="w-150 h-80 bg-gray-900 rounded-2xl p-6 text-white">
+        <div className="w-130 h-70 bg-gray-900 rounded-2xl p-6 text-white flex flex-col justify-between">
             {/* ATM component */}
             <div className="flex justify-between">
-                <h1 className="text-2xl font-bold mb-4">MasterCard</h1>
                 <div className="flex justify-start mb-4">
-                    <div className="w-12 h-12 bg-yellow-500 rounded-full"></div>
-                    <div className="w-12 h-12 bg-red-500 rounded-full -ml-4"></div>
+                    <div className="w-12 h-12 bg-red-500 rounded-full "></div>
+                    <div className="w-12 h-12 bg-yellow-500 rounded-full -ml-4"></div>
+                    <h1 className="text-2xl font-bold m-2">MasterCard</h1>
                 </div>
-                <h1 className="text-2xl font-bold mb-4">Chip</h1>
+                <img className="w-16 h-16 " src="src/assets/chip.png" alt="Chip"/>
+            </div>
+
+            <div className="flex justify-between">
+                <div>
+                    <p className="text-sm text-gray-400">Card Number</p>
+                    <p className="text-lg font-bold">5873 8903 5294 0836</p>
+                </div>
+            </div>
+            <div className="flex justify-between">
+                <div>
+                    <p className="text-sm text-gray-400">Card Holder</p>
+                    <p className="text-lg font-bold">John Doe</p>
+                </div>
+                <div>
+                    <p className="text-sm text-gray-400">Expires</p>
+                    <p className="text-lg font-bold">12/24</p>
+                </div>
             </div>
 
         </div>
